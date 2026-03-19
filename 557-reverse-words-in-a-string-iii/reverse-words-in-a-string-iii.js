387d5ad3845b02998b10c4679c628eec6ab40303
Caio <92585200+caioo105@users.noreply.github.com>
2026-03-19 10:29:48 -0300

    Time: 13 ms (28.35%) | Memory: 61.6 MB (40.53%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -2,14 +2,29 @@
  * @param {string} s
  * @return {string}
  */
+
 var reverseWords = function(s) {
-    return s
-        .split(" ")                     
-        .map(word => word.split("").reverse().join("")) 
-        .join(" ");                      
+    let chars = s.split('');
+    let start = 0;
+
+    for(let end = 0; end <= chars.length; end++){
+        if(end === chars.length || chars[end] === ' '){
+            reverse(chars, start, end - 1);
+
+            start = end + 1;
+        }
+    }
+
+    return chars.join('');
 };
 
+const reverse = function(chars, left, right){
+    while(left < right){
+        let temp = chars[left];
+        chars[left] = chars[right];
+        chars[right] = temp;
 
-console.log(reverseWords("Let's take LeetCode contest")); 
-console.log(reverseWords("Mr Ding"));
-
+        left++;
+        right--;
+    }
+};
