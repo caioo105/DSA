6f8e82267253dae6dceeb22ac214ba280f700ed4
Caio <92585200+caioo105@users.noreply.github.com>
2026-04-02 14:55:28 -0300

    Time: 52 ms (91.58%) | Memory: 55.8 MB (99.84%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.js
@@ -0,0 +1,30 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let count = 0;
+    
+    var dfs = function(grid, i, j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === "0") {
+            return;
+        }
+        grid[i][j] = "0";
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    };
+
+   
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === "1") {
+                count += 1;          
+                dfs(grid, i, j);     
+            }
+        }
+    }
+    
+    return count;
+};
